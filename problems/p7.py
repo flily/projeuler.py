a2df171ed228458cbb40a10f63ddda8f6635235f
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-11-19 21:27:20 +0800

    fix framework, multiple solutions for a problem is supported, and add p7

---
+++ problems/p7.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+"""
+10001st Prime
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+"""
+
+
+PID = 7
+ANSWER = 104743
+
+
+def solve_with_filter() -> int:
+    primes = [11, 13, 17, 19]
+    i = primes[-1] + 2
+    p3, p5, p7 = 2, 2, 6
+    while len(primes) < 9997:
+        p3 = (p3 + 1) % 3
+        p5 = (p5 + 1) % 5
+        p7 = (p7 + 1) % 7
+
+        if p3 == 0 or p5 == 0 or p7 == 0:
+            i += 2
+            continue
+
+        is_prime = True
+        for p in primes:
+            if i % p == 0:
+                is_prime = False
+                break
+
+            if p * p > i:
+                break
+
+        if is_prime:
+            primes.append(i)
+
+        i += 2
+
+    return primes[-1]
+
+
+# from line_profiler_pycharm import profile
+# @profile
+def solve_with_filter_and_prealloc_space() -> int:
+    primes = [0] * 9997
+    primes[0] = 11
+    primes[1] = 13
+    primes[2] = 17
+    primes[3] = 19
+    c = 4
+    i = 21
+    p3, p5, p7 = 2, 2, 6
+    while c < 9997:
+        p3 = (p3 + 1) % 3
+        p5 = (p5 + 1) % 5
+        p7 = (p7 + 1) % 7
+
+        if p3 == 0 or p5 == 0 or p7 == 0:
+            i += 2
+            continue
+
+        is_prime = True
+        j = 0
+        while j < c:
+            p = primes[j]
+            if i % p == 0:
+                is_prime = False
+                break
+
+            if p * p > i:
+                break
+
+            j += 1
+
+        if is_prime:
+            primes[c] = i
+            c += 1
+
+        i += 2
+
+    return primes[-1]
+
+
+def solve_naive() -> int:
+    primes = [3, 5, 7, 11, 13, 17, 19]
+    i = primes[-1]
+    while len(primes) < 10001:
+        is_prime = True
+        for p in primes:
+            if i % p == 0:
+                is_prime = False
+                break
+
+            if p * p > i:
+                break
+
+        if is_prime:
+            primes.append(i)
+
+        i += 2
+
+    return primes[-1]
+
+
+def solve_naive_with_prealloc_space() -> int:
+    primes = [3, 5, 7, 11, 13, 17, 19] + [0] * 9993
+    c = 7
+    i = primes[6] + 2
+    while c < 10000:
+        is_prime = True
+        for p in primes:
+            if i % p == 0:
+                is_prime = False
+                break
+
+            if p * p > i:
+                break
+
+        if is_prime:
+            primes[c] = i
+            c += 1
+
+        i += 2
+
+    return primes[-1]
+
+
+if __name__ == "__main__":
+    solve_with_filter_and_prealloc_space()
--- projeuler.py
+++ projeuler.py
@@ -9,14 +9,18 @@ Main entry, runner of all problem solutions.
 
 import os
 import sys
+import inspect
 import importlib
 import argparse
+import math
 from typing import (
     Callable,
     Generator,
     List,
+    Mapping,
 )
 from datetime import datetime
+from threading import Thread
 
 
 PROBLEM_DIR = "problems"
@@ -40,8 +44,9 @@ def _get_parser():
         "-c", "--check", action="store_true", help="check the solution answer"
     )
     cmd_run.add_argument(
-        "id", nargs="*", type=int, help="run specific problems"
+        "-t", "--timeout", type=float, default=1000.0, help="timeout in milliseconds"
     )
+    cmd_run.add_argument("id", nargs="*", type=int, help="run specific problems")
 
     return parser
 
@@ -52,17 +57,90 @@ def _add_indent(s: str, indent: str) -> str:
     return "\n".join(result)
 
 
+class _TimeOutResult:
+    pass
+
+
+class Result:
+    """
+    Result of a problem solution.
+    """
+
+    def __init__(self, result: int, time: float):
+        self.result = result
+        self.time = time
+
+    def __repr__(self) -> str:
+        return f"<{self.result}, {self.time}>"
+
+    def is_timeout(self) -> bool:
+        """
+        Check if this result is a timeout.
+        """
+        return self.time < 0.0
+
+
+def find_best_result(
+    default_result: Result, result_map: Mapping[str, Result]
+) -> tuple([bool, str]):
+    """
+    Find best result.
+    """
+    best_key = ""
+    best_result = -1.0
+
+    if default_result is not None:
+        if not default_result.is_timeout():
+            best_result = default_result.time
+
+    for key, result in result_map.items():
+        if result.is_timeout():
+            continue
+
+        if best_result < 0 or result.time < best_result:
+            best_key = key
+            best_result = result.time
+
+    if best_result < 0:
+        return False, ""
+
+    return True, best_key
+
+
+def run_solver(solver: Callable[[], int], timeout: float = 1000.0) -> Result:
+    """
+    Run a solver function.
+    """
+    result = _TimeOutResult()
+
+    def wrapper():
+        nonlocal result
+        result = solver()
+
+    thread = Thread(target=wrapper)
+    time_start = datetime.now()
+    thread.start()
+    thread.join(timeout=timeout / 1000)
+    time_finish = datetime.now()
+    if isinstance(result, _TimeOutResult):
+        return None, -1000.0
+
+    dt = 1000.0 * (time_finish - time_start).total_seconds()
+    return result, dt
+
+
 class ProblemSolver:
     """
     Base class of problem solution.
     """
 
+    solvers: Mapping[str, Callable[[], int]]
     __doc__ = ""
 
-    def __init__(self, pid: int, answer: int, solve: Callable[[], int]):
+    def __init__(self, pid: int, answer: int):
         self.pid = pid
         self.answer = answer
-        self.solve = solve
+        self.solvers = {}
         self.title = ""
         self.content = ""
 
@@ -77,6 +155,80 @@ class ProblemSolver:
 
         self.title = lines[0].strip()
         self.content = "\n".join(lines[1:]).strip()
+
+    def add_solver(self, name: str, func: Callable[[], int]):
+        """
+        Add a solver function.
+        """
+        if name in self.solvers:
+            raise RuntimeError(f"Solver {name} already exists")
+
+        self.solvers[name] = func
+
+    def print(self, key: str, result: Result, check: bool = False) -> str:
+        """
+        Print result of a solver.
+        """
+        pid = self.pid
+        r = ""
+        if result is not None:
+            r = result.result
+            if result.is_timeout():
+                r = "timeout"
+            elif result.result is None:
+                r = "no answer returned"
+
+        if key != "":
+            line = [f"{'':<5} {key:<40} {r:<10}"]
+        else:
+            line = [f"{pid:<5} {self.title:.<40} {r:<10}"]
+
+        if check:
+            if self.answer is None:
+                rc = "not set"
+
+            elif result is None:
+                rc = ""
+
+            elif result.is_timeout():
+                rc = "timeout"
+
+            elif self.answer == result.result:
+                rc = "correct"
+
+            else:
+                rc = "wrong"
+
+            line.append(f" {rc:8}")
+
+        if result is not None:
+            line.append(f" {math.fabs(result.time):10.3f}ms")
+        else:
+            line.append("             ")
+
+        return "".join(line)
+
+    def solve(
+        self, name: str = None, timeout: float = 1000.0
+    ) -> tuple[Result, Mapping[str, Result]]:
+        """
+        Solve the problem.
+        """
+        default_result = None
+        result_map = {}
+
+        for key, solver in self.solvers.items():
+            if name is not None and key != name:
+                continue
+
+            result, time_cost = run_solver(solver, timeout=timeout)
+            item = Result(result, time_cost)
+            if key == "":
+                default_result = item
+            else:
+                result_map[key] = item
+
+        return default_result, result_map
 
 
 def find_problem_solvers(
@@ -103,13 +255,22 @@ def find_problem_solvers(
 
         try:
             mod = importlib.import_module(f"{module_name}")
-            attrs = ["PID", "ANSWER", "solve"]
+            attrs = ["PID", "ANSWER"]
             for attr in attrs:
                 if not hasattr(mod, attr):
                     raise RuntimeError(f"Missing attribute {attr} in {module_name}")
 
-            solver = ProblemSolver(mod.PID, mod.ANSWER, mod.solve)
+            solver = ProblemSolver(mod.PID, mod.ANSWER)
             solver.set_document(mod.__doc__)
+            for name, func in inspect.getmembers(mod, inspect.isfunction):
+                if name == "solve":
+                    solver.add_solver("", func)
+
+                elif len(name) > 6 and name.startswith("solve_"):
+                    solver.add_solver(name[6:], func)
+
+                else:
+                    continue
 
             if len(id_set) > 0 and solver.pid not in id_set:
                 continue
@@ -134,43 +295,37 @@ def do_list(id_list: List[int], full: bool):
             print()
 
 
-def do_run(id_list: List[int], check: bool):
+def do_run(id_list: List[int], timeout: float, check: bool):
     """
     Run problems.
     """
     retcode = 0
     for problem in find_problem_solvers(PROBLEM_DIR, id_list=id_list):
-        time_start: datetime
-        time_finish: datetime
-
-        time_start = datetime.now()
-
-        answer = problem.solve()
-        if answer is None:
+        default_solver, other_solvers = problem.solve(timeout=timeout)
+        if default_solver is None and len(other_solvers) <= 0:
             print(f"{problem.pid:<5d} {problem.title:.<30} no answer returned")
             return
 
-        time_finish = datetime.now()
+        has_best, best_result = find_best_result(default_solver, other_solvers)
+        total_time = 0.0
+        header_line = ""
+        lines = []
 
-        line = [
-            f"{problem.pid:<5d} {problem.title:.<30} {answer:<10d}"
-        ]
+        header_line = problem.print("", default_solver, check=check)
+        if default_solver is not None:
+            total_time += math.fabs(default_solver.time)
 
-        if check:
-            if problem.answer is None:
-                result = "not set"
-            elif problem.answer == answer:
-                result = "correct"
-            else:
-                result = "wrong"
-                retcode = 1
+        for key, solver in other_solvers.items():
+            line = problem.print(key, solver, check=check)
+            if has_best and key == best_result:
+                line = line + " *[BEST]"
+            lines.append(line)
+            total_time += math.fabs(solver.time)
 
-            line.append(f" {result:8}")
-
-        dt = 1000.0 * (time_finish - time_start).total_seconds()
-        line.append(f" {dt:.3f}ms")
-
-        print("".join(line))
+        header_line += f" total={total_time:.3f}ms"
+        print(header_line)
+        for line in lines:
+            print(line)
 
     sys.exit(retcode)
 
@@ -186,7 +341,7 @@ def main():
         do_list(args.id, args.full)
 
     elif args.command == "run":
-        do_run(args.id, args.check)
+        do_run(args.id, args.timeout, args.check)
 
     else:
         parser.print_help()
