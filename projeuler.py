952edd644ebfee2f0694516f9ed2310367ff3da0
Flily Hsu <1126143+flily@users.noreply.github.com>
2023-11-28 21:55:28 +0800

    refactor framework

--- projeuler.py
+++ projeuler.py
@@ -12,7 +12,6 @@ import sys
 import inspect
 import importlib
 import argparse
-import math
 import time
 
 from threading import Thread
@@ -57,79 +56,106 @@ def _add_indent(s: str, indent: str) -> str:
     return "\n".join(result)
 
 
-class _TimeOutResult:
-    pass
-
-
-class Result:
-    """
-    Result of a problem solution.
-    """
-
-    def __init__(self, result: int, run_time: float):
-        self.result = result
-        self.time = run_time
-
+class _TimeoutResult:
     def __repr__(self) -> str:
-        return f"<{self.result}, {self.time}>"
+        return "TIMEOUT"
+
+
+class SolutionMethod:
+    """
+    Solution method
+    """
+
+    def __init__(self, func: Callable[[], int], name: str, note: str = ""):
+        self.func = func
+        self.name = name
+        self.note = note or ""
+        self.time_cost = 0.0
+        self.result = _TimeoutResult()
+        self.finished = False
+
+    def solve(self, timeout: float = 1000.0) -> None:
+        """
+        Run the solution method.
+        """
+        result = _TimeoutResult()
+        dt = 0.0
+        finished = False
+
+        def thread_main():
+            nonlocal result
+            nonlocal dt
+            nonlocal finished
+
+            time_start = time.perf_counter()
+            result = self.func()
+            time_finish = time.perf_counter()
+            dt = 1000.0 * (time_finish - time_start)
+            finished = True
+
+        thrad = Thread(target=thread_main)
+        thrad.start()
+        thrad.join(timeout=timeout / 1000)
+        if finished:
+            self.finished = True
+            self.result = result
+            self.time_cost = dt
+
+        else:
+            self.finished = False
+            self.result = None
+            self.time_cost = timeout
+
+    @property
+    def title(self) -> str:
+        """
+        Title of this method.
+        """
+        result = self.name
+        if self.note != "":
+            result = self.note.strip().split("\n")[0]
+
+        return result
 
     def is_timeout(self) -> bool:
         """
-        Check if this result is a timeout.
+        Is solution method timeout
         """
-        return self.time < 0.0
+        return not self.finished
 
+    def print(self, title: str, suffix: str | None, answer: int = None) -> str:
+        """
+        Print result of this method.
+        """
+        line = [f"{title}"]
 
-def find_best_result(
-    default_result: Result, result_map: Mapping[str, Result]
-) -> tuple([bool, str]):
-    """
-    Find best result.
-    """
-    best_key = ""
-    best_result = -1.0
+        if self.result is None:
+            r = "NO RESULT"
+        else:
+            r = f"{self.result}"
 
-    if default_result is not None:
-        if not default_result.is_timeout():
-            best_result = default_result.time
+        line.append(f" {r:<15}")
 
-    for key, result in result_map.items():
-        if result.is_timeout():
-            continue
+        if answer is not None:
+            if self.is_timeout():
+                rc = "timeout"
 
-        if best_result < 0 or result.time < best_result:
-            best_key = key
-            best_result = result.time
+            elif self.result is None:
+                rc = "NO ANSWER"
 
-    if best_result < 0:
-        return False, ""
+            elif answer == self.result:
+                rc = "correct"
 
-    return True, best_key
+            else:
+                rc = "wrong"
 
+            line.append(f" {rc:8}")
 
-def run_solver(solver: Callable[[], int], timeout: float = 1000.0) -> Result:
-    """
-    Run a solver function.
-    """
-    result = _TimeOutResult()
-    dt = 0.0
+        line.append(f" {self.time_cost:10.3f}ms")
+        if suffix is not None:
+            line.append(f" {suffix}")
 
-    def wrapper():
-        nonlocal result
-        nonlocal dt
-
-        time_start = time.perf_counter()
-        result = solver()
-        time_finish = time.perf_counter()
-        dt = 1000.0 * (time_finish - time_start)
-
-    thread = Thread(target=wrapper)
-    thread.start()
-    thread.join(timeout=timeout / 1000)
-    if isinstance(result, _TimeOutResult):
-        return None, -timeout
-
-    return result, dt
+        return "".join(line)
 
 
 class ProblemSolver:
@@ -137,13 +163,13 @@ class ProblemSolver:
     Base class of problem solution.
     """
 
-    solvers: Mapping[str, Callable[[], int]]
+    methods: Mapping[str, SolutionMethod]
     __doc__ = ""
 
     def __init__(self, pid: int, answer: int):
         self.pid = pid
         self.answer = answer
-        self.solvers = {}
+        self.methods = {}
         self.title = ""
         self.content = ""
         self.timeout = 0.0
@@ -160,79 +186,97 @@ class ProblemSolver:
         self.title = lines[0].strip()
         self.content = "\n".join(lines[1:]).strip()
 
-    def add_solver(self, name: str, func: Callable[[], int]):
+    def add_method(self, func: Callable[[], int], name: str, note: str = ""):
         """
-        Add a solver function.
+        Add a solution method.
         """
-        if name in self.solvers:
-            raise RuntimeError(f"Solver {name} already exists")
+        if name in self.methods:
+            raise RuntimeError(f"Method {name} already exists")
 
-        self.solvers[name] = func
+        method = SolutionMethod(func, name, note)
+        self.methods[name] = method
 
-    def print(self, key: str, result: Result, check: bool = False) -> str:
+    def is_corrent(self) -> bool:
         """
-        Print result of a solver.
+        Is the solution correct?
+        """
+        for method in self.methods.values():
+            if method.is_timeout():
+                return False
+
+            if method.result is None:
+                return False
+
+            if self.answer is not None and method.result != self.answer:
+                return False
+
+        return True
+
+    def find_best_solution(self) -> str:
+        """
+        Find the best solution.
+        """
+        cost = None
+        best = None
+        for name, method in self.methods.items():
+            if method.is_timeout():
+                continue
+
+            if method.result is None:
+                continue
+
+            if cost is None or method.time_cost < cost:
+                cost = method.time_cost
+                best = name
+
+        return best
+
+    def print(self, check: bool = False) -> str:
+        """
+        Print result of a problem solver.
         """
         pid = self.pid
-        r = ""
-        if result is not None:
-            r = result.result
-            if result.is_timeout():
-                r = "timeout"
-            elif result.result is None:
-                r = "no answer returned"
+        lines = []
 
-        if key != "":
-            line = [f"{'':<5} {key:<40} {r:<15}"]
-        else:
-            line = [f"{pid:<5} {self.title:.<40} {r:<15}"]
+        answer = self.answer
+        if not check:
+            answer = None
+        header = f"{pid:<5} {self.title:.<40}"
+        if len(self.methods) > 1:
+            best = self.find_best_solution()
+            total_cost = 0.0
+            for name, method in self.methods.items():
+                suffix = "*BEST" if name == best else None
+                title = f"      {method.title:.<40}"
+                line = method.print(title, suffix, answer=answer)
+                lines.append(line)
+                total_cost += method.time_cost
 
-        if check:
-            if self.answer is None:
-                rc = "not set"
-
-            elif result is None:
-                rc = ""
-
-            elif result.is_timeout():
-                rc = "timeout"
-
-            elif self.answer == result.result:
-                rc = "correct"
-
+            placeholder = ""
+            if check:
+                lines.append(header + f" {placeholder:24} {total_cost:10.3f}ms")
             else:
-                rc = "wrong"
+                lines.append(header + f" {placeholder:15} {total_cost:10.3f}ms")
 
-            line.append(f" {rc:8}")
+        elif len(self.methods) == 1:
+            method = list(self.methods.values())[0]
+            lines.append(method.print(header, None, answer=answer))
 
-        if result is not None:
-            line.append(f" {math.fabs(result.time):10.3f}ms")
         else:
-            line.append("             ")
+            header += " NO SOLUTION"
+            lines.append(header)
 
-        return "".join(line)
+        return "\n".join(lines)
 
-    def solve(
-        self, name: str = None, timeout: float = 1000.0
-    ) -> tuple[Result, Mapping[str, Result]]:
+    def solve(self, name: str = None, timeout: float = 1000.0) -> None:
         """
         Solve the problem.
         """
-        default_result = None
-        result_map = {}
-
-        for key, solver in self.solvers.items():
+        for key, method in self.methods.items():
             if name is not None and key != name:
                 continue
 
-            result, time_cost = run_solver(solver, timeout=timeout + self.timeout)
-            item = Result(result, time_cost)
-            if key == "":
-                default_result = item
-            else:
-                result_map[key] = item
-
-        return default_result, result_map
+            method.solve(timeout=timeout + self.timeout)
 
 
 def _natural_filename(filename: str) -> List[str | int]:
@@ -281,15 +325,16 @@ def import_solver(module_name: str) -> ProblemSolver:
 
     for name, func in inspect.getmembers(mod, inspect.isfunction):
         if name == "solve":
-            solver.add_solver("", func)
+            solver.add_method(func, "", func.__doc__)
 
         elif len(name) > 6 and name.startswith("solve_"):
-            solver.add_solver(name[6:], func)
+            solver.add_method(func, name[6:], func.__doc__)
 
         else:
             continue
 
     return solver
+
 
 def find_problem_solvers(
     dirname: str, id_list: List[int] = None
@@ -301,7 +346,9 @@ def find_problem_solvers(
 
     id_set = set(id_list or [])
     file_list = os.listdir(target_dir)
-    file_natural_list = [(filename, _natural_filename(filename)) for filename in file_list]
+    file_natural_list = [
+        (filename, _natural_filename(filename)) for filename in file_list
+    ]
     file_natural_list.sort(key=lambda x: x[1])
     for filename, _ in file_natural_list:
         if not filename.endswith(".py"):
@@ -345,31 +392,12 @@ def do_run(id_list: List[int], timeout: float, check: bool):
     """
     retcode = 0
     for problem in find_problem_solvers(PROBLEM_DIR, id_list=id_list):
-        default_solver, other_solvers = problem.solve(timeout=timeout)
-        if default_solver is None and len(other_solvers) <= 0:
-            print(f"{problem.pid:<5d} {problem.title:.<30} no answer returned")
-            return
+        problem.solve(timeout=timeout)
+        line = problem.print(check=check)
+        if check and not problem.is_corrent():
+            retcode = 1
 
-        has_best, best_result = find_best_result(default_solver, other_solvers)
-        total_time = 0.0
-        header_line = ""
-        lines = []
-
-        header_line = problem.print("", default_solver, check=check)
-        if default_solver is not None:
-            total_time += math.fabs(default_solver.time)
-
-        for key, solver in other_solvers.items():
-            line = problem.print(key, solver, check=check)
-            if has_best and key == best_result:
-                line = line + " *[BEST]"
-            lines.append(line)
-            total_time += math.fabs(solver.time)
-
-        header_line += f" total={total_time:.3f}ms"
-        print(header_line)
-        for line in lines:
-            print(line)
+        print(line)
 
     sys.exit(retcode)
 
