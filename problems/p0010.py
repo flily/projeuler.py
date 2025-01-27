b4089891548c000cf80869031d9a6f615236ba15
Flily Hsu <1126143+flily@users.noreply.github.com>
2025-01-27 13:55:19 +0800

    add solution with sieve for p10

--- problems/p0010.py
+++ problems/p0010.py
@@ -15,13 +15,16 @@ PID = 10
 ANSWER = 142913828922
 
 
+MAX_NUM = 2_000_000
+
+
 def solve_naive() -> int:
     basic_primes = [3, 5, 7, 11, 13, 17, 19]
     primes = basic_primes + ([0] * 220)
     prime_count = 7
     s = 2 + sum(basic_primes)
     i = primes[prime_count - 1] + 2
-    while i <= 2_000_000:
+    while i <= MAX_NUM:
         j, is_prime = 0, True
         while j < prime_count:
             p = primes[j]
@@ -36,10 +39,51 @@ def solve_naive() -> int:
 
         if is_prime:
             s += i
-            if i * i <= 2_000_000:
+            if i * i <= MAX_NUM:
                 primes[prime_count] = i
                 prime_count += 1
 
         i += 2
+
+    return s
+
+
+def solve_sieve_with_enumerate() -> int:
+    sieve = [True] * MAX_NUM
+    sieve[0] = sieve[1] = False
+    s = 0
+    for i, is_prime in enumerate(sieve):
+        if is_prime:
+            s += i
+            for j in range(i * i, MAX_NUM, i):
+                sieve[j] = False
+
+    return s
+
+
+def solve_sieve_with_iterate() -> int:
+    sieve = [True] * MAX_NUM
+    sieve[0] = sieve[1] = False
+    s = 0
+
+    i = 0
+    while i < MAX_NUM:
+        is_prime = sieve[i]
+        if is_prime:
+            s += i
+            for j in range(i * i, MAX_NUM, i):
+                sieve[j] = False
+
+    return s
+
+
+def solve_sieve_with_slice() -> int:
+    sieve = [True] * MAX_NUM
+    sieve[0] = sieve[1] = False
+    s = 0
+    for i, is_prime in enumerate(sieve):
+        if is_prime:
+            s += i
+            sieve[i * i:MAX_NUM:i] = [False] * len(range(i * i, MAX_NUM, i))
 
     return s
