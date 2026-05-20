9206fa1f5dec9acbe77900d58de5bd84a4ad91c3
Flily Hsu <1126143+flily@users.noreply.github.com>
2026-05-21 04:00:48 +0800

    fix solutions in p47, use the wrong method but get the correct solution

--- problems/p0047.py
+++ problems/p0047.py
@@ -22,10 +22,14 @@ first of these numbers?
 ANSWER = 134043
 TIMEOUT_EXT = 500.0
 
+
 def is_prime(n: int) -> bool:
     """
     Check if n (n >= 3) is prime.
     """
+    if n % 2 == 0:
+        return False
+
     i = 3
     while i * i <= n:
         if n % i == 0:
@@ -43,16 +47,18 @@ def find_prime_factors(n: int) -> set[int]:
     result = set()
     if n % 2 == 0:
         result.add(2)
+        if is_prime(n // 2):
+            result.add(n // 2)
 
     i = 3
     while i * i <= n:
         if n % i == 0:
             if is_prime(i):
                 result.add(i)
-            if is_prime(n // i):
+            if i * i != n and is_prime(n // i):
                 result.add(n // i)
 
-        i += 2
+        i += 1
 
     return result
 
@@ -73,3 +79,64 @@ def solve_naive() -> int:
         n += 1
 
     return n - 3
+
+
+def build_primes_list(max_prime: int) -> list[int]:
+    result = [2, 3, 5, 7, 11, 13, 17, 19]
+
+    i = 23
+    while i <= max_prime:
+        check_prime = True
+        for p in result:
+            if p * p > i:
+                break
+
+            if i % p == 0:
+                check_prime = False
+                break
+
+        if check_prime:
+            result.append(i)
+
+        i += 2
+
+    return result
+
+
+def find_prime_factors_count_with_prime_set(primes: list[int], n: int) -> int:
+    result = 0
+
+    for p in primes:
+        if p * 2 > n:
+            break
+
+        if n % p == 0:
+            result += 1
+
+    if result <= 0:
+        result = 1
+
+    return result
+
+
+def solve_prime_set() -> int:
+    n = 647
+    c = 0
+
+    primes = build_primes_list(1000)
+
+    while True:
+        pfs_count = find_prime_factors_count_with_prime_set(primes, n)
+
+        if pfs_count == 4:
+            c += 1
+        else:
+            c = 0
+
+        if c == 4:
+            break
+
+        n += 1
+
+    return n - 3
+
